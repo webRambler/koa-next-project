e55c44489bc146ffbd9e784145c90cf5ce3c5820
webRambler <talent1203@163.com>
2019-10-14 16:33:29 +0800

    feat：集成antd并按需加载，集成css及sass

--- components/nav.js
+++ components/nav.js
@@ -14,7 +14,7 @@ const Nav = () => (
     <ul>
       <li>
         <Link href='/'>
-          <a>Home</a>
+          <a className="main">Home</a>
         </Link>
       </li>
       {links.map(({ key, href, label }) => (
